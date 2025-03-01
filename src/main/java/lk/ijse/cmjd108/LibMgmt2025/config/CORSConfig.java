9fbafe3ec61ed2dea106b71e32d351b952ce924f
ThanuraSilva <thanurasilva1989@gmail.com>
2025-03-01 10:59:30 +0530

    Add Basic Auth to the application with Spring security

--- src/main/java/lk/ijse/cmjd108/LibMgmt2025/config/CORSConfig.java
+++ src/main/java/lk/ijse/cmjd108/LibMgmt2025/config/CORSConfig.java
@@ -2,22 +2,28 @@ package lk.ijse.cmjd108.LibMgmt2025.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
 @Configuration
 public class CORSConfig {
-    @Bean
-    public WebMvcConfigurer configurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                //add the configs
-                registry
-                        .addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
-    }
+   @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        var corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://lcoalhost:3000"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+        corsConfiguration.setAllowCredentials(true);
+
+       var source = new UrlBasedCorsConfigurationSource();
+       source.registerCorsConfiguration("/**", corsConfiguration);
+       return source;
+
+   }
 }
