2c08ac9119f85672f17aa77470686b19f9320f52
ThanuraSilva <thanurasilva1989@gmail.com>
2025-03-01 13:09:42 +0530

    Create UserEntity with implement UserDetails

---
+++ src/main/java/lk/ijse/cmjd108/LibMgmt2025/entities/secure/UserEntity.java
@@ -0,0 +1,41 @@
+package lk.ijse.cmjd108.LibMgmt2025.entities.secure;
+
+import jakarta.persistence.*;
+import lk.ijse.cmjd108.LibMgmt2025.dto.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class UserEntity implements Serializable, UserDetails {
+    @Id
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_"+role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}
